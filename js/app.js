78495f014e97632d974ce99c249aa86c661b2477
daCHEF <Doughboi85@gmail.com>
2024-10-30 19:17:32 -0400

    removed html inline css style from controls id, to css file resolving an error. Declared question const and defined variables for score, current question count and username.

--- js/app.js
+++ js/app.js
@@ -1,23 +1,38 @@
 /*------------ Users Stories -----------
-- AAU I want to know I'm on the right webpage when it loads.
-- AAU I want to start a new quiz game to test how smart I am.
-- AAU I want to see a title screen when I start the game.
-- AAU I want to choose my answers.
-- AAU I want to see my choice highlighted before I submit it.
-- AAU I want audio/visual feedback on whether my answer was correct.
-- AAU I want to know my updated score after each answer.
-- AAU I want to know when the game/round has ended.
+- #### MVP Goals
+
+  - AAU I want to know I'm on the right webpage when it loads.
+  - AAU I want to start a new quiz game to test how smart I am.
+  - AAU I want to see a title screen when I start the game.
+  - AAU I want to choose my answers.
+  - AAU I want to see my choice highlighted before I submit it.
+  - AAU I want audio/visual feedback on whether my answer was correct.
+  - AAU I want to know my updated score after each answer.
+  - AAU I want to know when the game/round has ended.
+
+- #### Stretch Goals
+
+  - AAU I want to add a custom Username
+  - AAU I want to choose from at least 2 - 3 categories.
+  - AAU I want to choose my difficulty
+  - AAU I want some background music while I play.
 */
 
 
 /*-------------- Constants -------------*/
+const questions = [
+    {
 
+    },
+]
 
 /*---------- Variables (state) ---------*/
-
+let score = 0;
+let currentQuestionIndex = 0;
+let username = '';
 
 /*----- Cached Element References  -----*/
-
+const questionTextEl = document.getElementById('option-one')
 
 /*-------------- Functions -------------*/
 
