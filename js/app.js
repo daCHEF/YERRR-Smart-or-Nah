c2a4a4355742ea71cf00c0540b5a724e610a630c
daCHEF <Doughboi85@gmail.com>
2024-10-30 17:08:22 -0400

    Boilerplated HTML, added JS script and linked CSS files. Created Header containing gmae title and user name input. Created score-container id displaying current user score and question. Question-container id for quiz questions.

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 /*------------ Users Stories -----------
-AAU: I want to start a new quiz game to test how smart I am.
-AAU: I want to see a title screen when I start the game.
-AAU: I want to choose my answers.
-AAU: I want to see my choice selected before I submit it.
-AAU: I want audio/visual feedback on whether my answer was correct.
-AAU: I want to know my updated score after each answer.
+- AAU I want to know I'm on the right webpage when it loads.
+- AAU I want to start a new quiz game to test how smart I am.
+- AAU I want to see a title screen when I start the game.
+- AAU I want to choose my answers.
+- AAU I want to see my choice highlighted before I submit it.
+- AAU I want audio/visual feedback on whether my answer was correct.
+- AAU I want to know my updated score after each answer.
+- AAU I want to know when the game/round has ended.
 */
 
 
