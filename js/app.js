b0b05d42be1528546341bb2df10666bf781fa3e0
daCHEF <Doughboi85@gmail.com>
2024-10-31 20:50:43 -0400

    Added code for right and wrong answers.

--- js/app.js
+++ js/app.js
@@ -26,12 +26,37 @@ const questions = [
         options: ['1765', '1804', '1636', '1592'],
         answer: 2
     },
+    
+    {
+        text: 'Do you see the issue?',
+        options: ['this', 'is', 'an', 'issue'],
+        answer: 2
+    },
+    
+    {
+        text: 'When was Brooklyn founded?',
+        options: ['1765', '1804', '1636', '1592'],
+        answer: 2
+    },
+    
+    {
+        text: 'When was Brooklyn founded?',
+        options: ['1765', '1804', '1636', '1592'],
+        answer: 2
+    },
+    
+    {
+        text: 'When was Brooklyn founded?',
+        options: ['1765', '1804', '1636', '1592'],
+        answer: 2
+    },
 ]
 
 /*---------- Variables (state) ---------*/
 let score = 0;
 let currentQuestion = 0;
 let username = '';
+let userSelected = -1;
 
 /*----- Cached Element References  -----*/
 const questionTextEl = document.getElementById('question-text');
@@ -66,12 +91,33 @@ function showQuestion() {
 function selectOption(optionId) {
     clearHighlight();
     optionsEl[optionId].classList.add('highlight');
+    userSelected = optionId;
 }
 
 function clearHighlight() {
     optionsEl.forEach((option) => {
         option.classList.remove('highlight');
+        option.classList.remove('rightAnswer');
+        option.classList.remove('wrongAnswer');
     });
 }
+function nextQuestion() { 
+    if (userSelected < 0) {
+//todo send user message
+        return;
+    }
+    clearHighlight();
+    if (userSelected == questions[currentQuestion].answer) {
+        optionsEl[userSelected].classList.add('rightAnswer');
+    } else {
+        optionsEl[userSelected].classList.add('wrongAnswer');
+    } 
+    userSelected = -1;
+    currentQuestion += 1;
+    showQuestion();
+}
+
+
+
 /*----------- Event Listeners ----------*/
 
